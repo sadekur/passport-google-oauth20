8911d241e40fbfd7d44322de273e4ec333c96c37
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-15 23:13:57 +0600

    feat: add active nav link highlighting based on current path

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success");
   res.locals.error_msg = req.flash("error");
   res.locals.user = req.user || null;
+  res.locals.path = req.path;
   next();
 });
 
